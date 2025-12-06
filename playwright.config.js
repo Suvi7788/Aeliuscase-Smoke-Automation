5ee25dd275b6b1628d7457d1f833779179904cfa
Suvi Diosn <suvidisonperera@gmail.com>
2025-12-06 11:48:02 +0530

    feat: implement event creation automation (EventForm component, test data)

--- playwright.config.js
+++ playwright.config.js
@@ -24,14 +24,18 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+  /* increase the wait time to 60 seconds */
+  timeout: 60000,
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    headless: false,
+    slowMo: 5000, // slows down actions by 1000ms = 1 second
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    headless: false,
     /* Add screenshot & video options */
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
