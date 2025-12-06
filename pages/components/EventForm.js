1fdd4fcd0a8c2b2cdeb18389c3b46cd5fea66693
Suvi Diosn <suvidisonperera@gmail.com>
2025-12-06 12:24:29 +0530

    feat: add updated selectors and improve event form filling

--- pages/components/EventForm.js
+++ pages/components/EventForm.js
@@ -2,19 +2,18 @@ class EventForm {
     constructor(page) {
         const caseFiled = "p-autocomplete[class='p-element p-inputwrapper ng-pristine ng-valid ng-star-inserted ng-untouched'] input[role='combobox']";
         const caseValue = ':text-is("AE00147 - Automation vs DO NOT DELETE")'
-        const subjectFiled = "input.p-inputtext.p-component.p-element.ng-pristine.ng-valid.ng-touched";
+        const subjectFiled = 'input[formcontrolname="subject"]';
         const assigneeFiled = "//p-autocomplete[@class='p-element p-inputwrapper ng-untouched ng-pristine ng-invalid']//input[@role='combobox']";
-        const assigneeValue = "suvi dison"
-        const descriptionFiled = "tdiv.ql-editor.ql-blank";
+        const descriptionFiled = ".ql-editor";
 
         this.page = page;
         this.TypeField = page.getByRole('combobox', { name: 'Select Type' });
         this.TypeValue = page.getByRole('option', { name: 'Appt With Attorney' });
-        this.CaseNo = page.locator(caseFiled);
+        this.CaseNo = page.getByRole('combobox', { name: 'Case No' });
         this.CaseValue = page.getByText(caseValue);
         this.Subject = page.locator(subjectFiled);
         this.Assignee = page.locator(assigneeFiled);
-        this.AssigneeValue = page.getByText(assigneeValue);
+        this.AssigneeValue = page.getByRole('option', { name: 'suvi dison' });
         this.Description = page.locator(descriptionFiled);
         this.SaveBtn = page.getByRole('button', { name: 'Save' });
     }
