b7ce10dbb65e0f99c24682c0dab52e73f8f435d4
Suvi Diosn <suvidisonperera@gmail.com>
2025-12-07 07:20:13 +0530

    feat: Add Playwright global login setup with secure .env integration
    - Implemented global setup to perform login once and save session in storageState.json
    - Secured credentials using environment variables
    - Streamlined login process for all tests

--- tests/createEvent.spec.js
+++ tests/createEvent.spec.js
@@ -7,23 +7,22 @@ const { Menu } = require("../pages/Menu");
 const { FirmEventListPage } = require("../pages/FirmEventListPage");
 const { CaseEventListPage } = require("../pages/CaseEventListPage");
 
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+});
+
+
 test.describe('Firm Dashboard Event Creation', () => {
     test('Create Firm Dashboard Event', async ({ page }) => {
-        const loginPage = new LoginPage(page);
         const firmDashboardPage = new FirmDashboardPage(page);
-        await loginPage.gotoLogin();
-        await loginPage.login('suvid', 'Aelius@1234');
         await firmDashboardPage.createFirmDashboardEvent(eventData.caseNo, eventData.Subject, eventData.Assignee, eventData.Description);
     })
 })
 
 test.describe('Case Dashboard Event Creation', () => {
     test('Create Case Dashboard Event', async ({ page }) => {
-        const loginPage = new LoginPage(page);
         const firmDashboardPage = new FirmDashboardPage(page);
         const caseDashboardPage = new CaseDashboardPage(page);
-        await loginPage.gotoLogin();
-        await loginPage.login('suvid', 'Aelius@1234');
         await firmDashboardPage.searchForCase(eventData.caseNo);
         await caseDashboardPage.createCaseDashboardEvent(eventData.caseNo, eventData.Subject, eventData.Assignee, eventData.Description);
     })
@@ -31,11 +30,8 @@ test.describe('Case Dashboard Event Creation', () => {
 
 test.describe('Firm Event List Event Creation', () => {
     test('Create Firm Event List Event', async ({ page }) => {
-        const loginPage = new LoginPage(page);
-        const menu = new Menu(page);
         const firmEventListPage = new FirmEventListPage(page);
-        await loginPage.gotoLogin();
-        await loginPage.login('suvid', 'Aelius@1234');
+        const menu = new Menu(page);
         await menu.navigateToFirmEventList();
         await firmEventListPage.createFirmEventListEvent(eventData.caseNo, eventData.Subject, eventData.Assignee, eventData.Description);
     })
@@ -44,12 +40,9 @@ test.describe('Firm Event List Event Creation', () => {
 
 test.describe('Case Event List Event Creation', () => {
     test('Create Case Event List Event', async ({ page }) => {
-        const loginPage = new LoginPage(page);
         const firmDashboardPage = new FirmDashboardPage(page);
         const caseDashboardPage = new CaseDashboardPage(page);
         const caseEventListPage = new CaseEventListPage(page);
-        await loginPage.gotoLogin();
-        await loginPage.login('suvid', 'Aelius@1234');
         await firmDashboardPage.searchForCase(eventData.caseNo);
         await caseDashboardPage.navigateToCaseEventList();
         await caseEventListPage.createCaseEventListEvent(eventData.caseNo, eventData.Subject, eventData.Assignee, eventData.Description);
