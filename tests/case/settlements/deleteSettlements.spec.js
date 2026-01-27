da4ef20d276876a6b01fed1b34c13f22a4cadd2c
Vinuka Ara <vinukaarampath2002@gmail.com>
2026-01-26 22:12:52 -0800

    add full settlements automation and update locators (add, edit, view, delete)

---
+++ tests/case/settlements/deleteSettlements.spec.js
@@ -0,0 +1,24 @@
+const { test } = require("@playwright/test");
+const SettlementSection = require("../../../pages/case/SettlementSection");
+const settlementData = require("../../../data/settlementData.json");
+const { Menu } = require("../../../pages/Menu");
+const CaseSummarySection = require("../../../pages/case/CaseSummarySection");
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+});
+
+test.describe('Delete Settlement', () => {
+    test('Delete Settlement', async ({ page }) => {
+        const menu = new Menu(page);
+        await menu.searchForCase(settlementData.caseNo);
+        const caseSummarySection = new CaseSummarySection(page);
+        await caseSummarySection.hoverOverInjuryDetails();
+        const settlementSection = new SettlementSection(page);
+        await settlementSection.openHasSettlement();
+        await settlementSection.deleteSettlement();
+        await settlementSection.deleteConfirm();
+        await settlementSection.deleteSuccessMsg(); 
+    })
+})
---
+++ tests/case/settlements/viewSettlements.spec.js
@@ -0,0 +1,22 @@
+const { test } = require("@playwright/test");
+const SettlementSection = require("../../../pages/case/SettlementSection");
+const settlementData = require("../../../data/settlementData.json");
+const { Menu } = require("../../../pages/Menu");
+const CaseSummarySection = require("../../../pages/case/CaseSummarySection");
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+});
+
+test.describe('View Settlements', () => {
+    test('View Settlements', async ({ page }) => {
+        const menu = new Menu(page);
+        await menu.searchForCase(settlementData.caseNo);
+        const caseSummarySection = new CaseSummarySection(page);
+        await caseSummarySection.hoverOverInjuryDetails();
+        const settlementSection = new SettlementSection(page);
+        await settlementSection.openHasSettlement();
+        await settlementSection.settlementView();
+    })
+})
