7e9356ed07946df683f701e413510283631f5f71
Vinuka Ara <vinukaarampath2002@gmail.com>
2026-02-05 01:07:07 -0800

    feat: add negotiation automation (add, delete, search, view)

---
+++ tests/case/settlements/negotiation/viewNegotiation .spec.js
@@ -0,0 +1,28 @@
+const { test } = require("@playwright/test");
+const SettlementSection = require("../../../../pages/case/SettlementSection");
+const settlementData = require("../../../../data/settlementData.json");
+const { Menu } = require("../../../../pages/Menu");
+const CaseSummarySection = require("../../../../pages/case/CaseSummarySection");
+const NegotiationSection = require("../../../../pages/case/NegotiationSection");
+
+
+test.beforeEach(async ({ page }) => {
+    await page.goto('/dashboard');
+});
+
+test.describe('Add New Negotiation', () => {
+    test.setTimeout(60000);
+    test('Add New Negotiation', async ({ page }) => {
+        const menu = new Menu(page);
+        const caseSummarySection = new CaseSummarySection(page);
+        const settlementSection = new SettlementSection(page);
+        const negotiationSection = new NegotiationSection(page);
+        await menu.searchForCase(settlementData.caseNo);
+        await caseSummarySection.hoverOverInjuryDetails();
+        await settlementSection.openNotHasSettlement();
+        await settlementSection.openNegotiations();
+        await negotiationSection.clickNegotiationOption();
+        await negotiationSection.clickViewNegotiation();
+        await negotiationSection.verifyViewNegotiation();
+    })
+})
