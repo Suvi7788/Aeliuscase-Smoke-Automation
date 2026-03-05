c6bb49e66d86af6831afa34c2b54390eefdf08b2
Vinuka Ara <vinukaarampath2002@gmail.com>
2026-03-04 23:19:16 -0800

    feat: add automation for referrals workflow, injury navigation, filters, contact list loading, and vocational service view; reported bugs in Azure DevOps

--- pages/case/TextMessage.js
+++ pages/case/TextMessage.js
@@ -8,6 +8,8 @@ class TextMessage {
         this.selectNameOption = page.getByRole('option', { name: 'test ara office number' });
         this.sendMessageBtn = page.getByRole('button', { name: 'Send' });
         this.verifySuccessMsgConfirm = page.locator('div.p-toast-detail', { hasText: 'Create new conversation successfully' });
+        this.contactListBtn = page.getByRole('button', { name: 'Contact List' });
+        this.contactListVerify = page.locator('button.p-button-icon-only.p-button-sm:has(.pi-comments)');
     }
     async addNewTextMessage() {
         await this.textMessageBtn.click();
@@ -23,6 +25,12 @@ class TextMessage {
     }
     async verifySuccessMsg(){
         await expect(this.verifySuccessMsgConfirm).toBeVisible();
+    }
+    async openContactList(){
+        await this.contactListBtn.click();
+    }
+    async verifyContactList(){
+        await expect(this.contactListVerify).toBeVisible();
     }
 
 
